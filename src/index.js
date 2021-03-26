b4caba00b97380111b432a8a424fe2e0ffe1203c
unknown <sjc1b@hotmail.es>
2021-03-26 19:57:05 +0100

    feat: react project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import Home from './components/Home';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { HashRouter, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+ReactDOM.render(<HashRouter>
+  <div>
+    <Route exact path="/" component={App} />
+    <Route path="/home" component={Home} /> 
+  </div>
+</HashRouter >, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
